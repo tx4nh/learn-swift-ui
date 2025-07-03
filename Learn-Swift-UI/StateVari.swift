438adaf19e9a593de28803f2f2bf4a05bd7b8471
Tuấngg Anhhh <atdevv2311@gmail.com>
2025-07-03 20:12:20 +0700

    rotate 3d effect

--- Learn-Swift-UI/StateVari.swift
+++ Learn-Swift-UI/StateVari.swift
@@ -11,13 +11,27 @@ struct StateVar: View {
         screenTap ? colors2 : colors1
     }
     
+    var text: String {
+        screenTap ? "I'm learning SwiftUI and it very complicated" : "Hello world"
+    }
+    
     var body: some View {
         ZStack{
             LinearGradient(colors: colors, startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
+            
+            Text(text)
+                .font(!screenTap ? .largeTitle : .title)
+                .foregroundStyle(screenTap ? .red : .white)
+                .multilineTextAlignment(.center)
+                .padding()
+                .rotation3DEffect(.degrees(screenTap ? 0 : 360.0), axis: (x: 0, y: 1, z: 1))
+            
         }
         .onTapGesture {
-            screenTap.toggle()
+            withAnimation(.easeIn(duration: 0.5)) {
+                screenTap.toggle()
+            }
         }
     }
 }
