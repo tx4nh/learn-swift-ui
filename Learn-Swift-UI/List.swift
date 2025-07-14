97c45ca11dbb12bd074ef42e340188ef2af88df1
Tuấngg Anhhh <atdevv2311@gmail.com>
2025-07-14 21:52:04 +0700

    listv view

---
+++ Learn-Swift-UI/List.swift
@@ -0,0 +1,18 @@
+//
+//  List.swift
+//  Learn-Swift-UI
+//
+//  Created by Tuấngg Anhhh on 14/7/25.
+//
+
+import SwiftUI
+
+struct List: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List()
+}
