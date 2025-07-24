36b06fcd63297f31d4d938b52f26612eea63e6f5
Tuấngg Anhhh <atdevv2311@gmail.com>
2025-07-24 08:39:56 +0700

    picker in form

--- Learn-Swift-UI/ExploringView.swift
+++ Learn-Swift-UI/ExploringView.swift
@@ -1,9 +1,16 @@
 import SwiftUI
 
+enum ProfileImageSize: String, CaseIterable{
+    case large = "Large"
+    case medium = "Medium"
+    case small = "Small"
+}
+
 struct ExploringView: View {
     @State private var bgColor = Color(.sRGB, red: 1, green: 1, blue: 1)
     @State private var date = Date()
     @State private var sliderValue: Double = 0.5
+    @State private var profileImagesizes: ProfileImageSize = .medium
     
     var body: some View {
         ZStack {
@@ -57,6 +64,18 @@ struct ExploringView: View {
                 } header: {
                     Text("Form")
                         .fontWeight(.semibold)
+                }
+                
+                Section {
+                    Picker("Profile Image Size", selection: $profileImagesizes) {
+                        Text("Large").tag(ProfileImageSize.large)
+                        Text("Medium").tag(ProfileImageSize.medium)
+                        Text("Small").tag(ProfileImageSize.small)
+                    }
+                    
+                    Button("Clear Image Cache") {}
+                } header: {
+                    Text("User Profiles")
                 }
             }
         }
